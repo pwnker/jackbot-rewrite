000b78a4c9dc9dd50c108c37ccf39ddef60790c9
Pwnker <github@pwnker.com>
2022-01-29 09:48:56 +0000

    added placeholder menu

---
+++ menus/sampleMenu.js
@@ -0,0 +1,10 @@
+const { MessageActionRow, MessageEmbed, MessageButton } = require('discord.js');
+const dotenv = require('dotenv');
+const { env } = require('process');
+dotenv.config();
+
+module.exports = {
+	customId: 'someMenu',
+	async execute(interaction) {
+
+}}
