e2315121b38cce14dc88b513ce2b1c88d13c94d7
pwnker <pwnker@pwnker.com>
2022-06-14 23:25:01 +0100

    remove invites intent

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_INVITES,
     Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGES,
     Intents.FLAGS.GUILD_VOICE_STATES,
   ],
 });
