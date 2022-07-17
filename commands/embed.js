32035e68721bd9d90e89a91e70e61d798e877ae0
pwnker <pwnker@pwnker.com>
2022-07-17 01:51:29 +0100

    whoops

--- commands/embed.js
+++ commands/embed.js
@@ -78,22 +78,22 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply({ ephemeral: true });
 
-        // const modRole = await interaction.client.db.settings.findOne({
-        //     attributes: ["value"],
-        //     where: { name: "modRole", guild: interaction.guild.id },
-        // });
+        const modRole = await interaction.client.db.settings.findOne({
+            attributes: ["value"],
+            where: { name: "modRole", guild: interaction.guild.id },
+        });
 
-        // if (
-        //     !interaction.member.roles.cache.some(
-        //         (role) => role.id === modRole?.value
-        //     ) &&
-        //     !interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
-        // ) {
-        //     return interaction.editReply({
-        //         content: "You do not have permission to use this command.",
-        //         ephemeral: true,
-        //     });
-        // }
+        if (
+            !interaction.member.roles.cache.some(
+                (role) => role.id === modRole?.value
+            ) &&
+            !interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
+        ) {
+            return interaction.editReply({
+                content: "You do not have permission to use this command.",
+                ephemeral: true,
+            });
+        }
 
         const username = interaction.options.getString("username");
         const avatar = interaction.options.getAttachment("avatar")?.url;
