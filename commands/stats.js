38fd3a9d03d5184cc433769f2e2b15bbcc1e4a03
pwnker <pwnker@pwnker.com>
2022-07-17 10:59:01 +0100

    Add stats command

---
+++ commands/stats.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Formatters } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("stats")
+        .setDescription("Display Jackbot's stats."),
+
+    async execute(interaction) {
+        client = interaction.client
+
+        members = 0
+        client.guilds.cache.forEach((guild) => { members += guild.memberCount })
+
+        const embed = new MessageEmbed()
+            .setTitle("Jackbot Stats")
+            .setColor("AQUA")
+            .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+            .setFooter({ text: `Watching over ${members} total members.` })
+            .addFields(
+                { name: "Servers", value: `${client.guilds.cache.size}`, inline: true },
+                { name: "Uptime", value: `${Math.floor(client.uptime / 86400000)} Days`, inline: true },
+            )
+
+        interaction.reply({ embeds: [embed], ephemeral: true })
+    }
+}
